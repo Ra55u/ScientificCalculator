51df12222e63e4cc58b4d35cf2bb6396a7dab478
Opilane TTHK <opilane@tthk.ee>
2024-01-29 12:18:31 +0200

    Commit

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace TARpe22Calc;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
